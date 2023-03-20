0a184ffc8270a641fabf3bf9ed91381c89961731
Elaidzha1940 <eli080293@gmail.com>
2023-03-20 17:10:11 +0700

    Range

--- Range Slider/Range Slider/ContentView.swift
+++ Range Slider/Range Slider/ContentView.swift
@@ -22,3 +22,25 @@ struct SliderComponent1_Previews: PreviewProvider {
             SliderComponent1()
     }
 }
+
+struct Slider1 : View {
+    
+    @State var width: CGFloat = 0
+    @State var width1:  CGFloat = 30
+    var totalWidth = UIScreen.main.bounds.width - 80
+    
+    var right1: String {
+            
+        let equal1 = self.width / self.totalWidth
+        
+        let km = Int(equal1 / 0.09)
+        return "\(km) км"
+    }
+    
+    var left1: String {
+        
+        let equal1 = self.width1 / self.totalWidth
+        
+        let km = Int(equal1 / 0.09)
+        return "\(km) км"
+    }
