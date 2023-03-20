d2e026658bf9d7491d04a5c678a44e871b8b5bb0
Elaidzha1940 <eli080293@gmail.com>
2023-03-20 17:06:45 +0700

    Range

---
+++ Range Slider/Range Slider/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Range Slider
+//
+//  Created by Elaidzha Shchukin on 20.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Range Slider/Range Slider/Range_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Range_SliderApp.swift
+//  Range Slider
+//
+//  Created by Elaidzha Shchukin on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Range_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
